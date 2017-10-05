9703baedce5bf158bbcbc73935cb509146b9f13a
Cody Sloan <chsloan@ncsu.edu>
2017-10-05 13:15:33 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Burger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Plain Hamburger');
